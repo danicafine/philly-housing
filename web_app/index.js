a15d59d5b23ae3c25ce81c5dd579da89cb14dc9a
harryhas29 <sharry@sas.upenn.edu>
2018-04-21 14:01:06 -0400

    adding css

--- web_app/index.js
+++ web_app/index.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 
 var app = express()
 app.set('port', (process.env.PORT || 5004));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/'));
 
 conn = mysql.createConnection({
 			  user     : 'mysql450',
@@ -11,12 +11,17 @@ conn = mysql.createConnection({
 			  port : 3306,
 			  host : 'mysql450.cgqpuarl2hxx.us-east-1.rds.amazonaws.com'});
 conn.query("USE mysql450")
-conn.query("show tables;", function (err, rows, fields) {
-	if (err) {console.log(err)}
-	else {
-		console.log(rows)
-	}
+
+app.get('/', function(request, response) {
+	response.sendFile(__dirname + '/' + 'index.html')
 })
+
+//conn.query("show tables;", function (err, rows, fields) {
+//	if (err) {console.log(err)}
+//	else {
+//		console.log(rows)
+//	}
+//})
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
   console.log(__dirname)
