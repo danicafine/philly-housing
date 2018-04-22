e654b3b49d5e49474c9637e7a9d4bcbc19e05f28
harryhas29 <sharry@sas.upenn.edu>
2018-04-22 15:17:43 -0400

    functioning q1

--- web_app/js/neighborhood_prices.js
+++ web_app/js/neighborhood_prices.js
@@ -1,11 +1,29 @@
 var app = angular.module('NeighborhoodViewer',[]);
 app.controller('nController', function($scope, $http) {
-	$.getJSON("/json/q1_header.json", function(json) {
-		console.log(json);
-		$scope.data = json;
-	})
-    $.getJSON("/json/q1_cache.json", function(json) {
-		console.log(json);
-    	$scope.data = json;
-	});
+	$scope.byRange = function (fieldName, minValue, maxValue) {
+	  if (minValue === undefined) minValue = Number.MIN_VALUE;
+	  if (maxValue === undefined) maxValue = Number.MAX_VALUE;
+	  if (minValue >= maxValue) {
+//		  minValue = Number.MIN_VALUE;
+		  maxValue = Number.MAX_VALUE;
+	  }
+	  return function predicateFunc(item) {
+		return minValue <= item[fieldName] && item[fieldName] <= maxValue;
+	  };
+	};
+    var request = $http.get("/json/q1_cache.json");
+    request.success(function(data) {
+        $scope.data = data;
+    });
+    request.error(function(data){
+        console.log('err');
+    });
+	
+	request = $http.get("/json/q1_header.json");
+    request.success(function(data) {
+        $scope.headers = data;
+    });
+    request.error(function(data){
+        console.log('err');
+    });
 });
